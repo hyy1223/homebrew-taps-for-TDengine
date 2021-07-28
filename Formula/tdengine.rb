dfa7471e358f477e82f8752c646c0fcb6ae3de8e
黄洪宇的项目必胜 <hhy@huanghongyudexiangmubishengdeMac.local>
2021-07-28 14:00:25 +0800

    one more

--- Formula/tdengine.rb
+++ Formula/tdengine.rb
@@ -12,7 +12,7 @@ class Tdengine < Formula
   depends_on "make"
 
   def install
-  system "tar -zxvf 7bdbd12d702c7cec7b3f1967387c60eed903839d3ec8f53a02c9efb3a62a9a50--TDengine-ver-2.1.5.0.tar.gztar.gz"
+  system "cd TDengine-ver-2.1.5.0"
   system "mkdir debug"
   system "cd debug"
   system "cmake .."
