f956eb3433cbe4a821d528fe8ea66cce233dae3b
Huang <hhy@HuangdeMac.local>
2021-08-09 16:18:44 -0700

    sass

--- Formula/tdengine.rb
+++ Formula/tdengine.rb
@@ -4,32 +4,32 @@
 class Tdengine < Formula
   desc "An open-source big data platform designed and optimized for the Internet of Things (IoT)."
   homepage "https://www.taosdata.com"
-  url "https://github.com/taosdata/TDengine/archive/refs/tags/ver-2.1.5.0.tar.gz"
-  sha256 "b5688928cde89c2a8bf315d11e1368843ea7d7d69c91ba1c95a5760e7b8bfb1d"
+  url "https://raw.githubusercontent.com/hyy1223/homebrew-taps-for-TDengine/main/Download/TDengine-201.tar.gz"
+  sha256 "7b49151746eceb3d300f4572a75f11296fd6a14973fece95c7b245287bf95b4a"
   license "AGPL-3.0"
 
-depends_on "cmake" => :build
+  depends_on "cmake" => :build
 
-  def install
-      
-       system "cmake",".",*std_cmake_args
-       system "cmake","--build","."
-       system "make"
-       system "make","install"
-       
-  
-  end
+ def install
+   
+     mkdir "debug" do
+     system "cmake", "..", *std_cmake_args
+     system "cmake","--build","."
+     system "make"
+     system "make","install"
+     end
+ end
 
-  #test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test TDengine`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    #system "false"
- # end
+ test do
+   # `test do` will create, run in and delete a temporary directory.
+   #
+   # This test will fail and we won't accept that! For Homebrew/homebrew-core
+   # this will need to be a test that verifies the functionality of the
+   # software. Run the test with `brew test TDengine-ver`. Options passed
+   # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+   #
+   # The installed folder is not in the path, so use the entire path to any
+   # executables being tested: `system "#{bin}/program", "do", "something"`.
+   system "false"
+ end
 end
