3725de9d849b9ecc22e997a94ec9a1584c9387ea
Huang <hhy@HuangdeMac.local>
2021-08-09 18:46:36 -0700

    ass

--- Formula/tdengine.rb
+++ Formula/tdengine.rb
@@ -2,30 +2,30 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Tdengine < Formula
-    desc "An open-source big data platform designed and optimized for the Internet of Things (IoT)."
-    homepage "https://www.taosdata.com"
-  url "https://raw.githubusercontent.com/hyy1223/homebrew-taps-for-TDengine/main/Download/TDengine02.tar.gz"
-  sha256 "c7d389a4c81475004947d9106eb22a5430cd9e8b75c8862390162deb998020a5"
-  license "AGPL-3.0"
+  desc ""
+  homepage ""
+  url "https://raw.githubusercontent.com/hyy1223/homebrew-taps-for-TDengine/main/Download/TDengine03.tar.gz"
+  sha256 "55f682479c0a9880ed1a881c96c03703966a2880e1ab6b508d8f2f8e8c275279"
+  license ""
 
-   depends_on "cmake" => :build
+  depends_on "cmake" => :build
 
-  def install
-    
-      mkdir "debug" do
-      system "cmake", "..", *std_cmake_args
-      system "cmake","--build","."
-      system "make"
-      system "make","install"
-      end
-  end
+ def install
+   
+     mkdir "debug" do
+     system "cmake", "..", *std_cmake_args
+     system "cmake","--build","."
+     system "make"
+     system "make","install"
+     end
+ end
 
   test do
     # `test do` will create, run in and delete a temporary directory.
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test TDengine-ver`. Options passed
+    # software. Run the test with `brew test TDengine`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
