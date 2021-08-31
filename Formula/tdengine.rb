61a85bf9ecdfc643f2bc268e8c91e2aaf9bd7c95
黄洪宇 <598002101@qq.com>
2021-08-31 09:52:55 -0700

    Formula for tdengine update

--- Formula/tdengine.rb
+++ Formula/tdengine.rb
@@ -2,8 +2,8 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Tdengine < Formula
-  desc ""
-  homepage ""
+  desc "An open-source big data platform designed and optimized for the Internet of Things (IoT)."
+  homepage "https://github.com/taosdata/TDengine"
   url "https://raw.githubusercontent.com/hyy1223/homebrew-taps-for-TDengine/main/Download/TDengine-2.1.5.0.tar.gz"
   sha256 "0ec55a84a02eb797cf32995d8aba0fc0f6e5f08e340a5026a59f1ee3af5ff42d"
   license "AGPL-3.0"
@@ -11,7 +11,6 @@ class Tdengine < Formula
   depends_on "cmake" => :build
 
  def install
-   
      mkdir "debug" do
      system "cmake", "..", *std_cmake_args
      system "cmake","--build","."
@@ -33,27 +32,13 @@ class Tdengine < Formula
      <string>#{plist_name}</string>
      <key>ProgramArguments</key>
      <array>
-       <string>/usr/local/cellar/tdengine/2.1.5.0/bin/taosd</string>
+       <string>/usr/local/cellar/tdengine/$(verNumber)/bin/taosd</string>
      </array>
      <key>RunAtLoad</key>
      <true/>
      <key>WorkingDirectory</key>
-     <string>/usr/local/cellar/tdengine/2.1.5.0/data</string>
+     <string>/usr/local/cellar/tdengine/$(verNumber)/data</string>
    </dict>
    </plist>
  EOS
  end
- 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test TDengine`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
-end
